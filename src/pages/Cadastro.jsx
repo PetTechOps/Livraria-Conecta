e12b62a4970e6ce37202d9062a59f496868ad57e
Rodrigo Mazucato <rodrigolopes.sp2005@gmail.com>
2024-10-25 22:12:07 -0300

    feat: implementação da página de Cadastro e seus componentes

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,10 +1,15 @@
 import { Header } from "../components/Header";
+import { FormLivro } from "../components/FormLivro";
 
 export function Cadastro() {
+  const infoLivro = ["Nome do Livro", "Autor", "Editora", "Ano de publicação"];
   return (
     <>
       <Header />
-      <h1>cadastro</h1>
+      <h1 style={{ color: "black", marginTop: "10rem", textAlign: "center" }}>
+        Cadastro de Usuário
+      </h1>
+      <FormLivro info={infoLivro} />
     </>
   );
 }
